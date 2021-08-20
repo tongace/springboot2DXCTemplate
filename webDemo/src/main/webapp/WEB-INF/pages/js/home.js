c660624d23384367ea46edbc9b6d419fd96757a6
tongace <chairat.it08@gmail.com>
2021-08-20 16:28:20 +0700

    add base 64 upload display download file

--- webDemo/src/main/webapp/WEB-INF/pages/js/home.js
+++ webDemo/src/main/webapp/WEB-INF/pages/js/home.js
@@ -1,6 +1,38 @@
 //<![CDATA[
+	'use strict'
+let HOME = (function ($) {
+	return {
+
+	}
+}(jQuery));
+
 $(document).ready(function() {
 	'use strict'
 	let a = '[[#{welcome.message}]]';
+
+	const blobToData = (blob) => {
+		return new Promise((resolve) => {
+		  const reader = new FileReader()
+		  reader.onloadend = () => resolve(reader.result)
+		  reader.readAsDataURL(blob)
+		})
+	  };
+
+	$('#uploadFile').on('change', async (e) => {
+		const file = e.target.files[0];
+		console.log(file);
+		let data = await blobToData(file);
+		console.log(data);
+		const uploadForm = {};
+		uploadForm.fileName = file.name;
+		uploadForm.fileData = data;
+		uploadForm.fileContentType = file.type;
+		$('#uploadPic').attr('src',data);
+		$('#uploadLink').attr('href',data);
+		$('#uploadLink').attr('download',file.name);
+		$('#uploadCard').show();
+		console.log(uploadForm);
+		$(e.target).val(null);
+	});
 });
 //]]>
