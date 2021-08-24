44bc05162caa0b511b846c681788eeea499d5f9d
unknown <ekekkarit@gmail.com>
2021-08-24 16:47:58 +0700

    update

---
+++ webDemo/src/main/webapp/WEB-INF/pages/js/persontest-call-api.js
@@ -0,0 +1,13 @@
+//<![CDATA[
+'use strict'
+let WDXC0001_API = (function ($) {
+    return {
+        getGimTypeCombo: () => DXCUtils.callAPI("/demo/combo/gimtypecombo","GET"),
+        getActiveFlagCombo: () => DXCUtils.callAPI("/demo/combo/activeflagcombo","GET"),
+        searchGimHeader: formData => DXCUtils.callAPI("/demo/gimmaster/gimheader","POST",formData),
+        saveGimHeader: formData => DXCUtils.callAPI("/demo/gimmaster/gimheader", "PUT",formData),
+        searchGimDetail: formData =>  DXCUtils.callAPI("/demo/gimmaster/gimdetail", "POST",formData),
+        saveGimDetail: formData =>  DXCUtils.callAPI("/demo/gimmaster/gimdetail", "PUT",formData)
+    }
+}(jQuery));
+//]]'>
