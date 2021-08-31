fb75829bac7501b5dbe8aed38acc71e9986ebb0b
tongace <chairat.it08@gmail.com>
2021-08-31 19:32:28 +0700

    update global exception

--- webDemo/src/main/java/com/dxc/application/exception/GlobalExceptionHandler.java
+++ webDemo/src/main/java/com/dxc/application/exception/GlobalExceptionHandler.java
@@ -1,14 +1,17 @@
 package com.dxc.application.exception;
 
+import com.dxc.application.constants.MessagesConstants;
 import com.dxc.application.exceptions.ApplicationException;
 import com.dxc.application.feature.common.dto.RestJsonData;
 import com.dxc.application.utils.MessageUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -28,6 +31,15 @@ public final class GlobalExceptionHandler {
         log.warn("Well known Error with message code {}", ae.getMessageCode());
         RestJsonData<String> restJsonData = new RestJsonData<>();
         restJsonData.setMessage(MessageUtil.getErrorMessage(messageSource, ae, request));
+        return restJsonData;
+    }
+
+    @ExceptionHandler({DuplicateKeyException.class})
+    @ResponseBody
+    public RestJsonData<String> applicationExceptionHandler(HttpServletRequest request, DuplicateKeyException ae) {
+        log.warn("Well known Error with message code insert database duplicated keys");
+        RestJsonData<String> restJsonData = new RestJsonData<>();
+        restJsonData.setMessage(messageSource.getMessage(MessagesConstants.DATA_DUPLICATED, new String[]{}, RequestContextUtils.getLocale(request)));
         return restJsonData;
     }
 
