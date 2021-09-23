081a0a86aa2352e0b4e3a8c26b48d376972bb079
tongace <chairat.it08@gmail.com>
2021-09-23 09:52:10 +0700

    update calculation screen

--- webDemo/src/main/webapp/WEB-INF/pages/calculation/calculation.js
+++ webDemo/src/main/webapp/WEB-INF/pages/calculation/calculation.js
@@ -25,13 +25,16 @@ $(document).ready(async function () {
     $('#step20Section').find(':input').prop('disabled', true);
     $('#step20Section').find('.button').addClass('disabled');
 
-    $('#step30Section').find(':input').prop('disabled', true);
-    $('#step30Section').find('.button').addClass('disabled');
+    $('#step30Section').find(':input').prop('disabled', false);
+    $('#step30Section').find('.button').removeClass('disabled');
 
-    $('#step32Section').find(':input').prop('disabled', false);
-    $('#step32Section').find('.button').removeClass('disabled');
+    // $('#step33Section').find(':input').prop('disabled', false);
+    // $('#step33Section').find('.button').removeClass('disabled');
 
-    $('#step40Section').find('.button').addClass('disabled');
-    $('#WDR01131IgnoreSimulation').removeClass('disabled');
+    $('#step40Section').find('.button').removeClass('disabled');
+    // $('#WDR01131IgnoreSimulation').removeClass('disabled');
+
+    $('#calculationSection').hide();
+    $('#initialSection').show();
 });
 //]]'>
